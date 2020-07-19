86a6fc9bb609f69446a234aba8e2a96dc63f674b
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-07-19 13:55:29 -0700

    switch to bent from request, switch to CommonJs

--- garlandtools-api.js
+++ garlandtools-api.js
@@ -1,66 +1,66 @@
 'use strict';
 
-const cron = require('cron').CronJob;
-const request = require('request-promise');
+import { CronJob } from 'cron';
+import bent from 'bent';
 
-//
-// Internal stuff.
-//
+const garlandTools = bent("https://www.garlandtools.org", "GET", "json", 200)
 
-module.exports.lang = "en"; // Can be altered directly.
+export const lang = "en"; // Can be altered directly.
 
-const achievementURL = `https://www.garlandtools.org/db/doc/achievement/${this.lang}/2/`;
-const achievementsURL = `https://www.garlandtools.org/db/doc/browse/${this.lang}/2/achievement.json`;
-const actionURL = `https://www.garlandtools.org/db/doc/action/${this.lang}/2/`;
-const actionsURL = `https://www.garlandtools.org/db/doc/browse/${this.lang}/2/action.json`;
-const dataURL = `https://www.garlandtools.org/db/doc/core/${this.lang}/3/data.json`;
-const endgameGearURL = `https://www.garlandtools.org/db/doc/equip/${this.lang}/2/end-`;
-const fateURL = `https://www.garlandtools.org/db/doc/fate/${this.lang}/2/`;
-const fatesURL = `https://www.garlandtools.org/db/doc/browse/${this.lang}/2/fate.json`;
-const fishingURL = `https://www.garlandtools.org/db/doc/browse/${this.lang}/2/fishing.json`;
-const iconURL = `https://www.garlandtools.org/files/icons/`;
-const instanceURL = `https://www.garlandtools.org/db/doc/instance/${this.lang}/2/`;
-const instancesURL = `https://www.garlandtools.org/db/doc/browse/${this.lang}/2/instance.json`;
-const itemURL = `https://www.garlandtools.org/db/doc/item/${this.lang}/3/`;
-const leveURL = `https://www.garlandtools.org/db/doc/leve/${this.lang}/3/`;
-const levesURL = `https://www.garlandtools.org/db/doc/browse/${this.lang}/2/leve.json`;
-const levelingGearURL = `https://www.garlandtools.org/db/doc/equip/${this.lang}/2/leveling-`;
-const mapURL = `https://www.garlandtools.org/files/maps/`;
-const mobURL = `https://www.garlandtools.org/db/doc/mob/${this.lang}/2/`;
-const mobsURL = `https://www.garlandtools.org/db/doc/browse/${this.lang}/2/mob.json`;
-const nodeURL = `https://www.garlandtools.org/db/doc/node/${this.lang}/2/`;
-const nodesURL = `https://www.garlandtools.org/db/doc/browse/${this.lang}/2/node.json`;
-const npcURL = `https://www.garlandtools.org/db/doc/npc/${this.lang}/2/`;
-const npcsURL = `https://www.garlandtools.org/db/doc/browse/${this.lang}/2/npc.json`;
-const searchURL = `https://www.garlandtools.org/api/search.php`;
-const statusURL = `https://www.garlandtools.org/db/doc/Status/${this.lang}/2/`;
-const statusesURL = `https://www.garlandtools.org/db/doc/browse/${this.lang}/2/status.json`;
-const questURL = `https://www.garlandtools.org/db/doc/quest/${this.lang}/2/`;
-const questsURL = `https://www.garlandtools.org/db/doc/browse/${this.lang}/2/quest.json`;
+const achievementURL = `/db/doc/achievement/${this.lang}/2/`;
+const achievementsURL = `/db/doc/browse/${this.lang}/2/achievement.json`;
+const actionURL = `/db/doc/action/${this.lang}/2/`;
+const actionsURL = `/db/doc/browse/${this.lang}/2/action.json`;
+const dataURL = `/db/doc/core/${this.lang}/3/data.json`;
+const endgameGearURL = `/db/doc/equip/${this.lang}/2/end-`;
+const fateURL = `/db/doc/fate/${this.lang}/2/`;
+const fatesURL = `/db/doc/browse/${this.lang}/2/fate.json`;
+const fishingURL = `/db/doc/browse/${this.lang}/2/fishing.json`;
+const iconURL = `/files/icons/`;
+const instanceURL = `/db/doc/instance/${this.lang}/2/`;
+const instancesURL = `/db/doc/browse/${this.lang}/2/instance.json`;
+const itemURL = `/db/doc/item/${this.lang}/3/`;
+const leveURL = `/db/doc/leve/${this.lang}/3/`;
+const levesURL = `/db/doc/browse/${this.lang}/2/leve.json`;
+const levelingGearURL = `/db/doc/equip/${this.lang}/2/leveling-`;
+const mapURL = `/files/maps/`;
+const mobURL = `/db/doc/mob/${this.lang}/2/`;
+const mobsURL = `/db/doc/browse/${this.lang}/2/mob.json`;
+const nodeURL = `/db/doc/node/${this.lang}/2/`;
+const nodesURL = `/db/doc/browse/${this.lang}/2/node.json`;
+const npcURL = `/db/doc/npc/${this.lang}/2/`;
+const npcsURL = `/db/doc/browse/${this.lang}/2/npc.json`;
+const searchURL = `/api/search.php`;
+const statusURL = `/db/doc/Status/${this.lang}/2/`;
+const statusesURL = `/db/doc/browse/${this.lang}/2/status.json`;
+const questURL = `/db/doc/quest/${this.lang}/2/`;
+const questsURL = `/db/doc/browse/${this.lang}/2/quest.json`;
 
 const cachedData = new Map(); // The cache, keyed with the URL. It stores a generic object value with properties data and time.
 
 var cacheTime = 3600000; // A new request is made at least an hour after the previous. These files don't change very often.
 
 const cache = async (url) => { // A simple function to check the cache for data.
+    const now = new Date().getTime();
     if (!cachedData.get(url)) {
         cachedData.set(url, {
-            data: await request(url),
-            time: now
+            data: await garlandTools(url),
+            time: now,
         });
     }
     return cachedData.get(url).data;
 };
 
 const clearCache = () => {
+    const now = new Date().getTime();
     cachedData.forEach((value, key, map) => {
-        if (new Date().getTime() >= value.time + cacheTime) { // Not a perfect check, but functional. Things can stay in the cache for a minimum of cacheTime ms, and a maximum of 2*cacheTime - 1 ms.
+        if (now >= value.time + cacheTime) { // Not a perfect check, but functional. Things can stay in the cache for a minimum of cacheTime ms, and a maximum of 2*cacheTime - 1 ms.
             cachedData.delete(key);
         }
     });
 };
 
-var cacheJob = new cron(new Date(cacheTime), clearCache);
+var cacheJob = new CronJob(new Date(cacheTime), clearCache);
 
 //
 // Public wrapper methods.
@@ -69,21 +69,22 @@ var cacheJob = new cron(new Date(cacheTime), clearCache);
 /**
  * Clears the cache.
  */
-module.exports.clearCache = () => {
+const _clearCache = () => {
     cachedData.forEach((value, key, map) => {
         cachedData.delete(key);
     });
 };
+export { _clearCache as clearCache };
 
 /**
  * Sets cache time.
  * @param time The time to cache data.
  */
-module.exports.setCacheTime = (time) => {
+export function setCacheTime(time) {
     cacheTime = time;
     cacheJob.stop();
-    cacheJob = new cron(new Date(cacheTime), clearCache);
-};
+    cacheJob = new CronJob(new Date(cacheTime), clearCache);
+}
 
 //
 // API accessors.
@@ -93,322 +94,322 @@ module.exports.setCacheTime = (time) => {
  * @param id The achievement's ID.
  * @returns An achievement's JSON listing.
  */
-module.exports.achievement = async (id) => {
+export async function achievement(id) {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(achievementURL + `${id}.json`));
+        response = (await cache(achievementURL + `${id}.json`));
         resolve(response);
     });
-};
+}
 
 /**
  * @returns The achievement JSON index.
  */
-module.exports.achievements = async () => {
+export async function achievements() {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(achievementsURL)).browse;
+        response = (await cache(achievementsURL)).browse;
         resolve(response);
     });
-};
+}
 
 /**
  * @param id The action's ID.
  * @returns An action's JSON listing.
  */
-module.exports.action = async (id) => {
+export async function action(id) {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(actionURL + `${id}.json`));
+        response = (await cache(actionURL + `${id}.json`));
         resolve(response);
     });
-};
+}
 
 /**
  * @returns The action JSON index.
  */
-module.exports.actions = async () => {
+export async function actions() {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(actionsURL)).browse;
+        response = (await cache(actionsURL)).browse;
         resolve(response);
     });
-};
+}
 
 /**
  * @returns The entirety of the JSON data index.
  */
-module.exports.data = async () => {
+export async function data() {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(dataURL));
+        response = (await cache(dataURL));
         resolve(response);
     });
-};
+}
 
 /**
  * @param job The three-letter job abbreviation.
  * @returns The endgame equipment for a job.
  */
-module.exports.endgameGear = async (job) => {
+export async function endgameGear(job) {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(endgameGearURL + `${job}.json`));
+        response = (await cache(endgameGearURL + `${job}.json`));
         resolve(response);
     });
-};
+}
 
 /**
  * @param id The FATE's ID.
  * @returns A FATE's JSON listing.
  */
-module.exports.fate = async (id) => {
+export async function fate(id) {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(fateURL + `${id}.json`));
+        response = (await cache(fateURL + `${id}.json`));
         resolve(response);
     });
-};
+}
 
 /**
  * @returns The FATE JSON index.
  */
-module.exports.fates = async () => {
+export async function fates() {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(fatesURL)).browse;
+        response = (await cache(fatesURL)).browse;
         resolve(response);
     });
-};
+}
 
 /**
  * @returns The fishing spot JSON index.
  */
-module.exports.fishingSpots = async () => {
+export async function fishingSpots() {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(fishingURL)).browse;
+        response = (await cache(fishingURL)).browse;
         resolve(response);
     });
-};
+}
 
 /**
  * @param type The database directory to retrieve from, e.g. "item", "action", etc.
  * @param id The icon ID.
  * @returns A PNG icon.
  */
-module.exports.icon = async (type, id) => {
+export async function icon(type, id) {
     return new Promise(async (resolve, reject) => {
         let response;
         response = await cache(iconURL + `${type}/${id}.png`);
         resolve(response);
     });
-};
+}
 
 /**
  * @param id The instance's ID.
  * @returns An instance's JSON listing.
  */
-module.exports.instance = async (id) => {
+export async function instance(id) {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(instanceURL + `${id}.json`));
+        response = (await cache(instanceURL + `${id}.json`));
         resolve(response);
     });
-};
+}
 
 /**
  * @returns The instance JSON index.
  */
-module.exports.instances = async () => {
+export async function instances() {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(instancesURL)).browse;
+        response = (await cache(instancesURL)).browse;
         resolve(response);
     });
-};
+}
 
 /**
  * @param id The item's ID.
  * @returns An item's JSON listing.
  */
-module.exports.item = async (id) => {
+export async function item(id) {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(itemURL + `${id}.json`));
+        response = (await cache(itemURL + `${id}.json`));
         resolve(response);
     });
-};
+}
 
 /**
  * @param id The leve's ID.
  * @returns A leve's JSON listing.
  */
-module.exports.leve = async (id) => {
+export async function leve(id) {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(leveURL + `${id}.json`));
+        response = (await cache(leveURL + `${id}.json`));
         resolve(response);
     });
-};
+}
 
 /**
  * @returns The levequest JSON index.
  */
-module.exports.leves = async () => {
+export async function leves() {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(levesURL)).browse;
+        response = (await cache(levesURL)).browse;
         resolve(response);
     });
-};
+}
 
 /**
  * @param job The three-letter job abbreviation.
  * @returns The leveling equipment for a job, sorted by level.
  */
-module.exports.levelingGear = async (job) => {
+export async function levelingGear(job) {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(levelingGearURL + `${job}.json`));
+        response = (await cache(levelingGearURL + `${job}.json`));
         resolve(response);
     });
-};
+}
 
 /**
  * @param zone The zone name. Some zones require a parent zone as well, such as "La Noscea/Lower La Noscea".
  * @returns A PNG map.
  */
-module.exports.map = async (zone) => {
+export async function map(zone) {
     return new Promise(async (resolve, reject) => {
         let response;
         response = await cache(mapURL + `${zone}.png`);
         resolve(response);
     });
-};
+}
 
 /**
  * @param id The mob's ID.
  * @returns A mob's JSON listing.
  */
-module.exports.mob = async (id) => {
+export async function mob(id) {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(mobURL + `${id}.json`));
+        response = (await cache(mobURL + `${id}.json`));
         resolve(response);
     });
-};
+}
 
 /**
  * @returns The mob JSON index.
  */
-module.exports.mobs = async () => {
+export async function mobs() {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(mobsURL)).browse;
+        response = (await cache(mobsURL)).browse;
         resolve(response);
     });
-};
+}
 
 /**
  * @param id The node's ID.
  * @returns A node's JSON listing.
  */
-module.exports.node = async (id) => {
+export async function node(id) {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(nodeURL + `${id}.json`));
+        response = (await cache(nodeURL + `${id}.json`));
         resolve(response);
     });
-};
+}
 
 /**
  * @returns The gathering node JSON index.
  */
-module.exports.nodes = async () => {
+export async function nodes() {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(nodesURL)).browse;
+        response = (await cache(nodesURL)).browse;
         resolve(response);
     });
-};
+}
 
 /**
  * @param id The NPC's ID.
  * @returns An NPC's JSON listing.
  */
-module.exports.npc = async (id) => {
+export async function npc(id) {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(npcURL + `${id}.json`));
+        response = (await cache(npcURL + `${id}.json`));
         resolve(response);
     });
-};
+}
 
 /**
  * @returns The NPC JSON index.
  */
-module.exports.npcs = async () => {
+export async function npcs() {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(npcsURL)).browse;
+        response = (await cache(npcsURL)).browse;
         resolve(response);
     });
-};
+}
 
 /**
  * @returns A JSON response for a search.
  */
-module.exports.search = async (query) => {
+export async function search(query) {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(searchURL + `?text=${query}&lang=${this.lang}`));
+        response = (await cache(searchURL + `?text=${query}&lang=${this.lang}`));
         resolve(response);
     });
-};
+}
 
 /**
  * @param id The status's ID.
  * @returns An status's JSON listing.
  */
-module.exports.status = async (id) => {
+export async function status(id) {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(statusURL + `${id}.json`));
+        response = (await cache(statusURL + `${id}.json`));
         resolve(response);
     });
-};
+}
 
 /**
  * @returns The status JSON index.
  */
-module.exports.statuses = async () => {
+export async function statuses() {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(statusesURL)).browse;
+        response = (await cache(statusesURL)).browse;
         resolve(response);
     });
-};
+}
 
 /**
  * @param id The quest's ID.
  * @returns A quest's JSON listing.
  */
-module.exports.quest = async (id) => {
+export async function quest(id) {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(questURL + `${id}.json`));
+        response = (await cache(questURL + `${id}.json`));
         resolve(response);
     });
-};
+}
 
 /**
  * @returns The quest JSON index.
  */
-module.exports.quests = async () => {
+export async function quests() {
     return new Promise(async (resolve, reject) => {
         let response;
-        response = JSON.parse(await cache(questsURL)).browse;
+        response = (await cache(questsURL)).browse;
         resolve(response);
     });
-};
+}
